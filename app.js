f27fea99e716cbf041204039424f5de6cb8d19a2
zhiguang <zhiguang53846@gmail.com>
2024-08-30 21:24:57 -0400

    finished most of the logic, leave styling

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
+const rgb = 255;
 const myLibrary = [];
 
-function Book(title,author,pages,read){
-this.title=title;
-this.author=author;
-this.pages=pages;
-this.read=read;
-let didRead = '';
-  this.info = function(){
-  if(read)
-  { didRead = "read"
-  }
-  else{
-    didRead = "not read"
-  }
-    return `${title} by ${author}, ${numPages} pages, ${didRead}`;
-  }
+function Book(title, author, pages, read, index, red, green, blue) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.index = index;
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+    let didRead = '';
+    this.info = function() {
+        if (read) {
+            didRead = "read"
+        } else {
+            didRead = "not read"
+        }
+        return `${title} by ${author}, ${numPages} pages, ${didRead}`;
+    }
 }
 
-function addBookToLibrary(title,author,pages,read){
-const book = new Book(title,author,pages,read);
-myLibrary.push(book);
+function addBookToLibrary(title, author, pages, read, index, red, green, blue) {
+    const book = new Book(title, author, pages, read, index, red, green, blue);
+    myLibrary.push(book);
+    return book;
 }
 
 const content = document.querySelector(".content");
@@ -33,13 +38,16 @@ const read = document.querySelector('#read');
 
 const newBookButton = document.querySelector(".newBookBtn");
 
-newBookButton.addEventListener("click", ()=>{
+newBookButton.addEventListener("click", () => {
     sidebar.appendChild(newBook);
 })
 
-newBook.onsubmit = function(event){
+newBook.onsubmit = function(event) {
     event.preventDefault();
-    addBookToLibrary(title.value, author.value, pages.value, read.value);
+    const r = rnd(rgb);
+    const g = rnd(rgb);
+    const b = rnd(rgb);
+    addBookToLibrary(title.value, author.value, pages.value, read.value, myLibrary.length, r, g, b);
     newBook.title.value = "";
     newBook.author.value = "";
     newBook.pages.value = "";
@@ -47,13 +55,52 @@ newBook.onsubmit = function(event){
     populateLibrary();
 }
 
-function populateLibrary(){
-for (item of myLibrary)
-{
-    const newBookElement = document.createElement('div');
-    console.log(item.title);
-    newBookElement.textContent = item.title;
-    newBookElement.classList.add('book');
-    content.appendChild(newBookElement);
+function populateLibrary() {
+    const bookShelf = document.querySelectorAll('.book');
+    for (let i = 0; i < bookShelf.length; i++) {
+        bookShelf[i].remove();
+    }
+    for (let j = 0; j < myLibrary.length; j++) {
+        const newBookElement = document.createElement('div');
+        const bookTitle = document.createElement('span');
+        const bookAuthor = document.createElement('span');
+        const bookRead = document.createElement('button');
+        newBookElement.classList.add('book');
+        bookAuthor.textContent = myLibrary[j].author;
+        bookAuthor.classList.add('bookText');
+        bookTitle.textContent = myLibrary[j].title;
+      bookTitle.classList.add('bookText');
+      if (myLibrary[j].read === 'Yes') {
+        bookRead.textContent = 'read';
+      }
+      else {
+        bookRead.textContent = 'not read';
+      }
+        newBookElement.appendChild(bookTitle);
+      newBookElement.appendChild(bookAuthor);
+        newBookElement.style.backgroundColor = `rgb(${myLibrary[j].red},${myLibrary[j].green},${myLibrary[j].blue})`;
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = `remove`;
+        removeBtn.addEventListener('click', () => {
+            myLibrary.splice(j, 1);
+            populateLibrary();
+        })
+      newBookElement.appendChild(removeBtn);
+      bookRead.addEventListener('click', () => {
+        if (myLibrary[j].read === 'Yes')
+        {
+          myLibrary[j].read = 'No';
+        }
+        else {
+          myLibrary[j].read = 'Yes';
+        }
+        populateLibrary();
+      })
+      newBookElement.appendChild(bookRead);
+        content.appendChild(newBookElement);
+    }
 }
+
+function rnd(val) {
+    return Math.floor(Math.random() * val);
 }
